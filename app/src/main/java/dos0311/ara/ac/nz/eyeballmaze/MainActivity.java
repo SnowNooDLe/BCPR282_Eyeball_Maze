0cb3ab4358e2ddd850ac2d081049ed6231b1283d
SnowNooDLe <sdw123123@gmail.com>
2019-06-08 00:31:20 +1200

    Replacing text to images

--- app/src/main/java/dos0311/ara/ac/nz/eyeballmaze/MainActivity.java
+++ app/src/main/java/dos0311/ara/ac/nz/eyeballmaze/MainActivity.java
@@ -3,6 +3,7 @@ package dos0311.ara.ac.nz.eyeballmaze;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import dos0311.ara.ac.nz.eyeballmaze.Model.*;
@@ -10,7 +11,8 @@ import dos0311.ara.ac.nz.eyeballmaze.Model.*;
 public class MainActivity extends AppCompatActivity {
     private int size = 6;
     Board board = new Board(size);
-    TextView[][] textViews = new TextView[6][6];
+    ImageView[][] imageViews = new ImageView[6][6];
+    int[][] imageSrcs = new int[6][6];
     TextView textViewForGoal;
 
 
@@ -23,51 +25,53 @@ public class MainActivity extends AppCompatActivity {
         textViewForGoal = findViewById(R.id.textView);
 
         //      first row
-        textViews[0][0] = findViewById(R.id.textView2);
-        textViews[0][1] = findViewById(R.id.textView3);
-        textViews[0][2] = findViewById(R.id.textView4);
-        textViews[0][3] = findViewById(R.id.textView5);
-        textViews[0][4] = findViewById(R.id.textView6);
-        textViews[0][5] = findViewById(R.id.textView7);
+        imageViews[0][0] = findViewById(R.id.imageView);
+        imageViews[0][1] = findViewById(R.id.imageView2);
+        imageViews[0][2] = findViewById(R.id.imageView3);
+        imageViews[0][3] = findViewById(R.id.imageView4);
+        imageViews[0][4] = findViewById(R.id.imageView5);
+        imageViews[0][5] = findViewById(R.id.imageView6);
+
+//        imageSrcs[0][0] = R.drawable.Empty_Block;
         //      second row
-        textViews[1][0] = findViewById(R.id.textView8);
-        textViews[1][1] = findViewById(R.id.textView9);
-        textViews[1][2] = findViewById(R.id.textView10);
-        textViews[1][3] = findViewById(R.id.textView11);
-        textViews[1][4] = findViewById(R.id.textView12);
-        textViews[1][5] = findViewById(R.id.textView13);
+        imageViews[1][0] = findViewById(R.id.imageView7);
+        imageViews[1][1] = findViewById(R.id.imageView8);
+        imageViews[1][2] = findViewById(R.id.imageView9);
+        imageViews[1][3] = findViewById(R.id.imageView10);
+        imageViews[1][4] = findViewById(R.id.imageView11);
+        imageViews[1][5] = findViewById(R.id.imageView12);
 
         //      third row
-        textViews[2][0] = findViewById(R.id.textView14);
-        textViews[2][1] = findViewById(R.id.textView15);
-        textViews[2][2] = findViewById(R.id.textView16);
-        textViews[2][3] = findViewById(R.id.textView17);
-        textViews[2][4] = findViewById(R.id.textView18);
-        textViews[2][5] = findViewById(R.id.textView19);
+        imageViews[2][0] = findViewById(R.id.imageView13);
+        imageViews[2][1] = findViewById(R.id.imageView14);
+        imageViews[2][2] = findViewById(R.id.imageView15);
+        imageViews[2][3] = findViewById(R.id.imageView17);
+        imageViews[2][4] = findViewById(R.id.imageView17);
+        imageViews[2][5] = findViewById(R.id.imageView18);
 
         //      fourth row
-        textViews[3][0] = findViewById(R.id.textView20);
-        textViews[3][1] = findViewById(R.id.textView21);
-        textViews[3][2] = findViewById(R.id.textView22);
-        textViews[3][3] = findViewById(R.id.textView23);
-        textViews[3][4] = findViewById(R.id.textView24);
-        textViews[3][5] = findViewById(R.id.textView25);
+        imageViews[3][0] = findViewById(R.id.imageView19);
+        imageViews[3][1] = findViewById(R.id.imageView20);
+        imageViews[3][2] = findViewById(R.id.imageView21);
+        imageViews[3][3] = findViewById(R.id.imageView22);
+        imageViews[3][4] = findViewById(R.id.imageView23);
+        imageViews[3][5] = findViewById(R.id.imageView24);
 
         //      fifth row
-        textViews[4][0] = findViewById(R.id.textView26);
-        textViews[4][1] = findViewById(R.id.textView27);
-        textViews[4][2] = findViewById(R.id.textView28);
-        textViews[4][3] = findViewById(R.id.textView29);
-        textViews[4][4] = findViewById(R.id.textView30);
-        textViews[4][5] = findViewById(R.id.textView31);
+        imageViews[4][0] = findViewById(R.id.imageView25);
+        imageViews[4][1] = findViewById(R.id.imageView26);
+        imageViews[4][2] = findViewById(R.id.imageView27);
+        imageViews[4][3] = findViewById(R.id.imageView28);
+        imageViews[4][4] = findViewById(R.id.imageView29);
+        imageViews[4][5] = findViewById(R.id.imageView30);
 
         //      sixth row
-        textViews[5][0] = findViewById(R.id.textView32);
-        textViews[5][1] = findViewById(R.id.textView33);
-        textViews[5][2] = findViewById(R.id.textView34);
-        textViews[5][3] = findViewById(R.id.textView35);
-        textViews[5][4] = findViewById(R.id.textView36);
-        textViews[5][5] = findViewById(R.id.textView37);
+        imageViews[5][0] = findViewById(R.id.imageView31);
+        imageViews[5][1] = findViewById(R.id.imageView32);
+        imageViews[5][2] = findViewById(R.id.imageView33);
+        imageViews[5][3] = findViewById(R.id.imageView34);
+        imageViews[5][4] = findViewById(R.id.imageView35);
+        imageViews[5][5] = findViewById(R.id.imageView36);
     }
 
 //    Starting the game and always start with Stage 1.
@@ -76,11 +80,12 @@ public class MainActivity extends AppCompatActivity {
 
         board.setGoal(0, 3);
         textViewForGoal.setText("Number of Goal(s) : " + board.numberOfGoals);
-        for (int i =0; i < size; i++){
-            for (int j = 0; j < size; j++){
-                textViews[i][j].setText(board.map[i][j]);
-            }
-        }
+//        This must be for programatically setup, will do manual first
+//        for (int i =0; i < size; i++){
+//            for (int j = 0; j < size; j++){
+//
+//            }
+//        }
     }
 
 }
